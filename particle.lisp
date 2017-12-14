5af20ba8d108e67b05d7514c45f414c8966b4e71
jfjhh <alex.striff1@gmail.com>
2017-12-13 23:32:09 -0800

    spline stuff

---
+++ particle.lisp
@@ -0,0 +1,240 @@
+;;;;
+;;;; Particle motion.
+;;;; Alex Striff
+;;;;
+
+(defclass particle ()
+  ((pos :initform (grid:make-foreign-array
+		   'double-float
+		   :initial-contents '(0d0 0d0))
+	:initarg :pos
+	:accessor pos
+	:type 'grid:vector-double-float)))
+
+(defclass generator ()
+  ((ndims :initform 2
+	  :initarg :ndims
+	  :accessor ndims
+	  :type 'integer)
+   (npoints :initarg :npoints
+	    :accessor npoints
+	    :type 'integer)
+   (dims :accessor dims
+	 :type 'simple-vector)))
+
+(defclass splinerator (generator)
+  ((nevals :initarg :nevals
+	   :accessor nevals
+	   :type 'integer)
+   (gsplines :accessor gsplines
+	   :type 'simple-vector)
+   (sdims :accessor sdims
+	  :type 'simple-vector)
+   (knots :accessor knots
+	  :type 'grid:vector-double-float)))
+
+(defmethod initialize-instance :after ((g generator) &key)
+  (with-slots (ndims npoints dims) g
+    (setf dims (make-array ndims))
+    (loop :for i :below ndims :do
+       (setf (aref dims i)
+	     (grid:make-foreign-array 'double-float
+				      :dimensions npoints)))))
+
+(defmethod initialize-instance :after ((s splinerator) &key)
+  (with-slots (npoints nevals sdims ndims gsplines knots) s
+    (setf sdims (make-array ndims))
+    (setf gsplines (make-array ndims))
+    (setf knots (grid:make-foreign-array 'double-float :dimensions npoints))
+    (loop :for i :below ndims :do
+       (setf (aref sdims i)
+	     (grid:make-foreign-array 'double-float
+				      :dimensions nevals)))))
+
+(defclass rot-particle (particle)
+  ((vel :initform 0d0
+	:initarg :vel
+	:accessor vel
+	:type 'double-float)
+   (theta :initform 0d0
+	  :initarg :theta
+	  :accessor theta
+	  :type 'double-float)
+   (omega :initform 0d0
+	  :initarg :omega
+	  :accessor omega
+	  :type 'double-float)))
+
+(defgeneric generate (g dimfuncs &key start end)
+  (:documentation "Generates g with the vector results of dimfuncs on [0,1]."))
+
+(defgeneric draw (p s &key)
+  (:documentation "Draws the particle p on the screen s."))
+
+(defgeneric particle-collide (p)
+  (:documentation "Collides the particle with the boundaries."))
+
+(defgeneric update-particle (p dt)
+  (:documentation "Updates the particle p with time step dt."))
+
+(defmethod generate ((g generator) dimfuncs &key (start 0d0) (end 1d0))
+  (with-slots (ndims npoints dims) g
+    (loop :for i :from 0 :below npoints :do
+       (let ((u (float (lerp (/ i (1- npoints)) start end) 0d0)))
+	 (loop :for j :from 0 :below ndims :do
+	    (setf (grid:aref (aref dims j) i)
+		  (funcall (aref dimfuncs j) u)))))))
+
+(defmethod generate :after ((s splinerator) dimfuncs &key (start 0d0) (end 1d0))
+  (declare (ignore dimfuncs))
+  (with-slots (dims sdims ndims npoints nevals gsplines knots) s
+    (loop :for i :from 0 :below npoints :do
+       (setf (grid:aref knots i)
+	     (float (lerp (/ i (1- npoints)) start end) 0d0)))
+    (setf gsplines
+	  (map
+	   'vector
+	   (curry #'gsll:make-spline
+		  gsll:+cubic-spline-interpolation+
+		  knots)
+	   dims))
+    (loop :for i :from 0 :below ndims :do
+       (loop :for j :from 0 :below nevals :do
+	  (setf (grid:aref (aref sdims i) j)
+		(gsll:evaluate
+		 (aref gsplines i)
+		 (float (lerp (/ j (1- nevals)) start end) 0d0)))))))
+
+(defparameter *cparam* 0d0)
+
+(defun multi-lerp (v &rest interpolants)
+  (let ((len (length interpolants)))
+    (multiple-value-bind (k u) (floor (* v (1- len)))
+      (let ((c (nthcdr k interpolants)))
+	(lerp u (car c) (cadr c))))))
+
+(defmethod draw ((p particle) (s sdl-screen) &key)
+  (with-slots (width height) s
+    (let* ((coords (screen-pos p s))
+	   (px (car coords))
+	   (py (cadr coords)))
+      (setf *cparam* (mod (+ *cparam* 0.005d0) 1d0))
+      (draw-out-circle px py))))
+
+(defmethod draw ((g generator) (s sdl-screen) &key (hr 1/2) (pmin nil) (pmax nil))
+  (with-slots (width height) s
+    (with-slots (dims ndims npoints) g
+      (let* ((coords
+	      (map
+	       'vector
+	       (lambda (d)
+		 (let ((pmin (or pmin (loop :for i :from 0 :below npoints
+					 :minimizing (grid:aref d i))))
+		       (pmax (or pmax (loop :for i :from 0 :below npoints
+					 :maximizing (grid:aref d i)))))
+		   (grid:map-grid :source d
+				  :element-function
+				  (lambda (x) (screen-pos x s :min pmin :max pmax)))))
+	       dims)))
+	(setf *cparam* (mod (+ *cparam* 0.005d0) 1d0))
+	(loop :for i :from 0 :below npoints :do
+	   (let ((cs (loop :for j :from 0 :below 2
+			:collecting
+			(let ((coord (grid:aref (aref coords j) i)))
+			  (if (= j 1) (- height coord) coord)))))
+	     (draw-out-circle (car cs) (cadr cs) hr)))))))
+
+(defmethod draw ((p splinerator) (s sdl-screen) &key (hr 1/2) (base nil) (clear nil))
+  (declare (ignore hr))
+  (when clear
+    (sdl:clear-display (sdl:color)))
+  (with-slots (width height) s
+    (with-slots (sdims ndims nevals) p
+      (let* ((pmin nil)
+	     (pmax nil)
+	     (coords
+	      (map
+	       'vector
+	       (lambda (d)
+		 (setf pmin (loop :for i :from 0 :below nevals
+			       :minimizing (grid:aref d i)))
+		 (setf pmax (loop :for i :from 0 :below nevals
+			       :maximizing (grid:aref d i)))
+		 (grid:map-grid :source d
+				:element-function
+				(lambda (x) (screen-pos x s :min pmin :max pmax))))
+	       sdims))
+	     (oldcs (loop :for j :from 0 :below 2
+		       :collecting
+		       (let ((coord (grid:aref (aref coords j) 0)))
+			 (if (= j 1) (- height coord) coord)))))
+	(loop :for i :from 1 :below nevals :do
+	   (let ((newcs
+		  (loop :for j :from 0 :below 2
+		     :collecting
+		     (let ((coord (grid:aref (aref coords j) i)))
+		       (if (= j 1) (- height coord) coord)))))
+					;(draw-out-circle (car oldcs) (cadr oldcs) hr)
+	     (draw-out-line (car oldcs) (cadr oldcs)
+			    (car newcs) (cadr newcs))
+	     (setf oldcs newcs)))
+	(when base
+	  (call-next-method p s :hr 2
+			    ;:pmin pmin :pmax pmax
+			    ))))))
+
+(defun draw-out-circle (x y &optional (hr 1/2))
+  (setf *cparam* (mod (+ *cparam* 0.005d0) 1d0))
+  (sdl:draw-filled-circle
+   (sdl:point :x x :y y)
+   (* hr 2)
+   :color (sdl:color :r (floor (multi-lerp *cparam* 255 0   0   255))
+		     :g (floor (multi-lerp *cparam* 0   0   255 0))
+		     :b (floor (multi-lerp *cparam* 0   255 0   0)))))
+
+(defun draw-out-line (x1 y1 x2 y2)
+  (setf *cparam* (mod (+ *cparam* 0.001d0) 1d0))
+  (sdl:draw-line
+   (sdl:point :x x1 :y y1)
+   (sdl:point :x x2 :y y2)
+   :color (sdl:color :r (floor (multi-lerp *cparam* 255 0   0   255))
+		     :g (floor (multi-lerp *cparam* 0   0   255 0))
+		     :b (floor (multi-lerp *cparam* 0   255 0   0)))))
+
+(defmethod particle-collide ((p rot-particle))
+  (with-slots (pos vel theta omega) p
+    (let ((x (grid:aref pos 0))
+	  (y (grid:aref pos 1)))
+      ;; Wall wrapping.
+      (when (< x -1d0)
+	(setf (grid:aref pos 0)  1d0))
+      (when (> x 1d0)
+	(setf (grid:aref pos 0) -1d0))
+      (when (< y -1d0)
+	(setf (grid:aref pos 1)  1d0))
+      (when (> y 1d0)
+	(setf (grid:aref pos 1) -1d0))
+
+      #||
+      ;; Wall bouncing.
+      (when (< x -1d0)
+	(setf (grid:aref pos 0) -1d0)
+	(setf theta (- pi theta)))
+      (when (> x 1d0)
+	(setf (grid:aref pos 0)  1d0)
+	(setf theta (- pi theta)))
+      (when (< y -1d0)
+	(setf (grid:aref pos 1) -1d0)
+	(setf theta (- tau theta)))
+      (when (> y 1d0)
+	(setf (grid:aref pos 1)  1d0)
+	(setf theta (- tau theta)))
+      ||#
+      )))
+
+(defmethod update-particle ((p rot-particle) (dt number))
+  (with-slots (pos vel theta omega) p
+    (setf theta (mod (+ theta (* omega dt)) tau))
+    (incf (grid:aref pos 0) (* vel (cos theta)))
+    (incf (grid:aref pos 1) (* vel (sin theta)))
+    (particle-collide p)))
--- sdl2.lisp
+++ sdl2.lisp
@@ -5,13 +5,51 @@
 
 (in-package #:mitty)
 
+(defparameter *width* 1024)
+(defparameter *height* 1024)
+
 (defparameter *xs* '(0 1/4 1/2 3/4 1))
 (defparameter *ys* '(0 1/4 1/2 3/4 1))
 
-(defparameter *px* 5/10)
-(defparameter *py* 5/10)
-(defparameter *vx* 1/100)
-(defparameter *vy* 1/100)
+(defparameter *time* 0d0)
+(defparameter *frame* 0)
+
+(defparameter *bullet* nil)
+
+(defparameter *bscreen*
+  (make-instance 'sdl-screen
+		 :width *width*
+		 :height *height*))
+
+(defun reset ()
+  (setf *time* 0d0)
+  (setf *bullet*
+	(make-instance 'rot-particle
+		       :pos (grid:make-foreign-array
+			     'double-float
+			     :initial-contents '(0d0 0d0))
+		       :vel 0.01d0
+		       :omega 0d0
+		       :theta (+ 0.1 (float (/ pi 2) 0d0)))))
+
+(defun func->bspline (a b n f &optional (nverts (* n 2)))
+  (let* ((fstep (/ (- b a) n))
+	 (xarr
+	  (grid:make-foreign-array
+	   'double-float
+	   :initial-contents
+	   (loop :for i :from a :below b :by fstep :collecting i)))
+	 (yarr
+	  (grid:make-foreign-array
+	   'double-float
+	   :initial-contents
+	   (loop :for i :from a :below b :by fstep :collecting (funcall f i))))
+	 (spline (gsll:make-spline gsll:+cubic-spline-interpolation+ xarr yarr)))
+    (let* ((begin (grid:aref xarr 0))
+	   (end (grid:aref xarr (1- n)))
+	   (vstep (/ (- end begin) nverts)))
+      (loop :for xi :from begin :below end :by vstep
+	 :collecting (list xi (gsll:evaluate spline xi))))))
 
 (defun draw-verts (xs ys)
   (loop :for x :in xs :for y :in ys :do
@@ -21,57 +59,33 @@
       :color (sdl:color :r 128 :g 0 :b 128)
       :stroke-color (sdl:color :r 0 :g 0 :b 255))))
 
-(defun update-particle (dt vx vy)
-  (let ((x (+ *px* (* dt vx) (* 0.1 (- 0.5 (random 1.0)))))
-	(y (+ *py* (* dt vy) (* 0.1 (- 0.5 (random 1.0))))))
-    (setf vx (- vx))
-    (setf vy (- vy))
-    (setf *px*
-	  (cond ((< x 0) 0)
-		((> x 1) 1)
-		(t (setf vx (- vx)) x)))
-    (setf *py*
-	  (cond ((< y 0) 0)
-		((> y 1) 1)
-		(t (setf vy (- vy)) y)))
-    (values vx vy)))
-
-(defun draw-particle (width height)
-  (let ((hr 1))
-    (sdl:draw-filled-circle
-     (sdl:point :x (- (* *px* width) hr)
-		:y (- (* *py* height) hr))
-     (* hr 2)
-     :color (sdl:color :r (- 255 (random 128)) 
-		       :g (- 255 (random 128))
-		       :b (- 255 (random 128))))))
+(defun draw-verts* (ps)
+  (loop :for p :in ps :do
+     (let ((x (car p))
+	   (y (cadr p)))
+       (sdl:draw-filled-circle
+	(sdl:point :x  x :y y)
+	2
+	:color (sdl:color :r 128 :g 0 :b 128)
+	:stroke-color (sdl:color :r 0 :g 0 :b 255)))))
 
 (defun to-points (xs ys)
   (mapcar (lambda (x y) (sdl:point :x x :y y)) xs ys))
 
-(defun bezier ()
-  (let ((width 512)
-	(height 512))
+(defun run-sdl-thread (&optional (func #'bezier) (name "Mitty"))
+  (bt:make-thread func :name name))
+
+(defmethod bezier ()
+  (let ((width *width*)
+	(height *height*))
     (sdl:with-init ()
       (sdl:window width height :title-caption "Mitty | SDL2 Test")
       (setf (sdl:frame-rate) 60)
       (sdl:clear-display (sdl:color :r 0 :g 0 :b 0))
       (sdl:with-surface (disp sdl:*default-display*)
 	(sdl:with-color (col (sdl:color :r 255 :g 255 :b 255))
-	  (loop :for i :from 0 :to (/ width 2) :by (/ width 20) :do
-	     (sdl:draw-bezier
-	      (list (sdl:point :x (- (* 9/20 width) (/ i 2.0))
-			       :y (+ 20 i))
-		    (sdl:point :x (*  21/20 width)
-			       :y (* 1/20 height))
-		    (sdl:point :x (* 11/10 width)
-			       :y (* 3/4 height))
-		    (sdl:point :x (- (* 3/5 width) (/ i 8.0))
-			       :y (+ (* 3/4 height) (/ i 4.0))))
-	      :segments 64))
 	  (sdl:clear-display (sdl:color :r 0 :g 0 :b 0))
-	  ))
-
+	  (reset)))
       (sdl:update-display)
       (sdl:with-events ()
 	(:quit-event () t)
@@ -81,21 +95,25 @@
 			   (sdl:push-quit-event)))
 	(:video-expose-event () (sdl:update-display))
 	(:idle ()
-	       (sdl:draw-box (sdl:rectangle :x 0 :y 0 :w width :h height)
-			     :color (sdl:color :r 0 :g 0 :b 0) :alpha 8)
-	       (let* ((xs (loop :repeat 4 :collecting (random 1.0)))
-		      (ys (loop :repeat 4 :collecting (random 1.0)))
-		      (vxs (mapcar (curry #'* width) xs))
-		      (vys (mapcar (curry #'* height) ys)))
-		 #||
-		 (sdl:draw-bezier (to-points vxs vys)
-				  :color (sdl:color :r (random 256)
-						    :g (random 256)
-						    :b (random 256)))
-		 ||#
-		 ;(draw-verts vxs vys)
-		 (multiple-value-bind (vx* vy*) (update-particle 1 *vx* *vy*)
-		   (setf *vx* vx*)
-		   (setf *vy* vy*))
-		 (draw-particle width height)
-		 (sdl:update-display)))))))
+	       ;; Run the loop but also keep SLIME working for interactivity.
+	       #||
+	       (let ((connection
+	       (or swank::*emacs-connection* (swank::default-connection))))
+	       (when (and connection
+	       (not (eql swank:*communication-style* :spawn)))
+	       (swank::handle-requests connection t)))
+	       ||#
+
+	       ;; Main stuff.
+	       ;; Blanking Box
+	       ;(sdl:draw-box (sdl:rectangle :x 0 :y 0 :w width :h height) :color (sdl:color :r 0 :g 0 :b 0) :alpha 8)
+
+	       #||
+	       ;; Particle stuff.
+	       (incf *time* 0.05d0)
+	       (setf (slot-value *bullet* 'omega) (expt *time* 2))
+	       (update-particle *bullet* 0.1d0)
+	       (draw-particle *bullet* *bscreen*)
+	       ||#
+	       ;(when (zerop (setf *frame* (mod (1+ *frame*) 10))))
+	       (sdl:update-display))))))
